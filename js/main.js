ceff42113b6e04c23f2f48b382dd9ca3de2c47ef
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-04-28 16:03:27 +0200

    add ticket program

--- js/main.js
+++ js/main.js
@@ -4,3 +4,37 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+//1. chiedere età passeggero
+let age = Number(prompt('inserisci età'));
+console.log(age);
+while (isNaN(age)) {
+  alert('Il valore inserito non è numerico');
+  age = Number(prompt('inserisci età'));
+  console.log(age);
+}
+
+//2. chiedere km che si vogliono effettuare
+let km = Number(prompt('inserire quanti km si vogliono effettuare'));
+console.log(km);
+while (isNaN(km)) {
+  alert('Il valore inserito non è numerico');
+  km = Number(prompt('inserire quanti km si vogliono effettuare'));
+  console.log(km);
+}
+
+//3. definire il prezzo del biglietto moltiplicando i km * 0.21
+let ticketPrice = km * 0.21;
+console.log(ticketPrice);
+
+//4. SE minorenni applicare il 20% di sconto SE over 65 applicare il 40%
+if (age < 18) {
+  let sold = (ticketPrice * 20) / 100;
+  ticketPrice = ticketPrice - sold;
+  console.log(ticketPrice.toFixed(2));
+} else if (age > 65) {
+  let sold = (ticketPrice * 40) / 100;
+  ticketPrice = ticketPrice - sold;
+  console.log(ticketPrice.toFixed(2));
+}
+//N.B output prezzo finale con massimo due cifre decimali
